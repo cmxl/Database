3387be6967869ae8000e7009abfe6117643352a1
Tobias Haimerl <th.cmxl@gmail.com>
2020-07-13 13:32:29 +0200

    Update average_index_fragmentation.sql

--- average_index_fragmentation.sql
+++ average_index_fragmentation.sql
@@ -1,11 +1,13 @@
-SELECT OBJECT_NAME(ips.OBJECT_ID)
- ,i.NAME
- ,ips.index_id
- ,index_type_desc
- ,avg_fragmentation_in_percent
- ,avg_page_space_used_in_percent
- ,page_count
+SELECT DB_NAME(ips.database_id) as [Database]
+ ,OBJECT_NAME(ips.OBJECT_ID) as [Table]
+ ,i.NAME as [Index]
+ ,ips.index_id as [Index ID]
+ ,index_type_desc as [Index Type]
+ ,avg_fragmentation_in_percent as [AVG Fragmentation (%)]
+ ,avg_page_space_used_in_percent as [AVG Page Space Used (%)]
+ ,page_count as [Pages]
 FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'SAMPLED') ips
 INNER JOIN sys.indexes i ON (ips.object_id = i.object_id)
  AND (ips.index_id = i.index_id)
+where ips.database_id > 4
 ORDER BY avg_fragmentation_in_percent DESC
