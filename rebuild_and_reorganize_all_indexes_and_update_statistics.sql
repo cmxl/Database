a5dbb72d657150f99a7a3116808ec1a6f16eee81
Tobias Haimerl <th.cmxl@gmail.com>
2020-07-13 21:33:26 +0200

    Update rebuild_and_reorganize_all_indexes_and_update_statistics.sql

--- rebuild_and_reorganize_all_indexes_and_update_statistics.sql
+++ rebuild_and_reorganize_all_indexes_and_update_statistics.sql
@@ -4,74 +4,55 @@ DECLARE @indexName NVARCHAR(500)
 DECLARE @indexId INT
 DECLARE @indexType NVARCHAR(55)
 DECLARE @percentFragment DECIMAL(11, 2)
-DECLARE @spaceUsed DECIMAL(11,2)
+DECLARE @spaceUsed DECIMAL(11, 2)
 DECLARE @pageCount BIGINT
+DECLARE @maxFragmentation INT = 30
 
-DECLARE FragmentedTableList CURSOR
-FOR
-SELECT 
-	OBJECT_NAME(ips.OBJECT_ID) as TableName
-	,i.Name as IndexName
-	,ips.index_id
-	,ips.index_type_desc as IndexType
-	,ips.avg_fragmentation_in_percent
-	,ips.avg_page_space_used_in_percent
-	,ips.page_count
+DECLARE FragmentedTableList CURSOR FOR
+SELECT OBJECT_NAME(ips.OBJECT_ID) as TableName,
+    i.Name as IndexName,
+    ips.index_id,
+    ips.index_type_desc as IndexType,
+    ips.avg_fragmentation_in_percent,
+    ips.avg_page_space_used_in_percent,
+    ips.page_count
 FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'SAMPLED') ips
-INNER JOIN sys.indexes i 
-	ON (ips.object_id = i.object_id)
-	AND (ips.index_id = i.index_id)
-WHERE
-	ips.avg_fragmentation_in_percent > @fragmentation
-	AND i.Name IS NOT NULL
-ORDER BY 
-	avg_fragmentation_in_percent DESC
+    INNER JOIN sys.indexes i ON (ips.object_id = i.object_id)
+    AND (ips.index_id = i.index_id)
+WHERE ips.avg_fragmentation_in_percent > @fragmentation
+    AND i.Name IS NOT NULL
+ORDER BY avg_fragmentation_in_percent DESC 
 
-
-OPEN FragmentedTableList
+OPEN FragmentedTableList 
 
 FETCH NEXT
-FROM FragmentedTableList
-INTO @tableName
-	,@indexName
-	,@indexId
-	,@indexType
-	,@percentFragment
-	,@spaceUsed
-	,@pageCount
-
-WHILE @@FETCH_STATUS = 0
-BEGIN
-	PRINT 'Processing [' + @indexName + '] on table [' + @tableName + '] which is ' + cast(@percentFragment AS NVARCHAR(50)) + '% fragmented'
-
-	IF (@percentFragment <= 30)
-	BEGIN
-		EXEC ('ALTER INDEX [' + @indexName + '] ON [' + @tableName + '] REBUILD; ')
-
-		PRINT 'Finished reorganizing [' + @indexName + '] on table [' + @tableName + ']'
-	END
-	ELSE
-	BEGIN
-		EXEC ('ALTER INDEX [' + @indexName + '] ON [' + @tableName + '] REORGANIZE;')
-
-		PRINT 'Finished rebuilding [' + @indexName + '] on table [' + @tableName + ']'
-	END
-
-	FETCH NEXT
-	FROM FragmentedTableList
-	INTO @tableName
-		,@indexName
-		,@indexId
-		,@indexType
-		,@percentFragment
-		,@spaceUsed
-		,@pageCount
+FROM FragmentedTableList INTO @tableName,
+    @indexName,
+    @indexId,
+    @indexType,
+    @percentFragment,
+    @spaceUsed,
+    @pageCount WHILE @@FETCH_STATUS = 0 BEGIN PRINT 'Processing [' + @indexName + '] on table [' + @tableName + '] which is ' + cast(@percentFragment AS NVARCHAR(50)) + '% fragmented' IF (@percentFragment <= @maxFragmentation) BEGIN EXEC (
+        'ALTER INDEX [' + @indexName + '] ON [' + @tableName + '] REBUILD; '
+    ) PRINT 'Finished rebuilding [' + @indexName + '] on table [' + @tableName + ']'
 END
+ELSE BEGIN EXEC (
+    'ALTER INDEX [' + @indexName + '] ON [' + @tableName + '] REORGANIZE;'
+) PRINT 'Finished reorganizing [' + @indexName + '] on table [' + @tableName + ']'
+END 
 
-CLOSE FragmentedTableList
+FETCH NEXT
+FROM FragmentedTableList INTO @tableName,
+    @indexName,
+    @indexId,
+    @indexType,
+    @percentFragment,
+    @spaceUsed,
+    @pageCount
+END 
 
+CLOSE FragmentedTableList 
 DEALLOCATE FragmentedTableList
-GO
-
+GO 
 EXEC sp_updatestats
 GO
