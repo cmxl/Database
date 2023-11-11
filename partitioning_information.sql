39df5e6fbd81d9b5af64ccdca32ee50e40832578
Tobias Haimerl <th.cmxl@gmail.com>
2023-11-11 16:00:21 +0100

    Create partitioning_information.sql

---
+++ partitioning_information.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT o.name as table_name, f.name as partition_function_name, rv.value as partition_range, fg.name as file_groupName, p.partition_number, p.rows as number_of_rows
+FROM sys.partitions p
+INNER JOIN sys.indexes i ON p.object_id = i.object_id AND p.index_id = i.index_id
+INNER JOIN sys.objects o ON p.object_id = o.object_id
+INNER JOIN sys.partition_schemes ps ON ps.data_space_id = i.data_space_id
+INNER JOIN sys.partition_functions f ON f.function_id = ps.function_id
+INNER JOIN sys.destination_data_spaces dds ON dds.partition_scheme_id = ps.data_space_id AND dds.destination_id = p.partition_number
+INNER JOIN sys.filegroups fg ON dds.data_space_id = fg.data_space_id
+LEFT OUTER JOIN sys.partition_range_values rv ON f.function_id = rv.function_id AND p.partition_number = rv.boundary_id
