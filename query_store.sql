447b3edc398f7aa6d4eeb643971513f054c13869
Tobias Haimerl <th.cmxl@gmail.com>
2025-01-09 11:33:13 +0100

    Create query_store.sql

---
+++ query_store.sql
@@ -0,0 +1,14 @@
+SELECT Txt.query_text_id, Txt.query_sql_text, Pln.plan_id, Qry.*, RtSt.*
+FROM sys.query_store_plan AS Pln
+INNER JOIN sys.query_store_query AS Qry
+    ON Pln.query_id = Qry.query_id
+INNER JOIN sys.query_store_query_text AS Txt
+    ON Qry.query_text_id = Txt.query_text_id
+INNER JOIN sys.query_store_runtime_stats RtSt
+ON Pln.plan_id = RtSt.plan_id;
+
+SELECT actual_state_desc, desired_state_desc, current_storage_size_mb,
+    max_storage_size_mb, readonly_reason, interval_length_minutes,
+    stale_query_threshold_days, size_based_cleanup_mode_desc,
+    query_capture_mode_desc
+FROM sys.database_query_store_options;
